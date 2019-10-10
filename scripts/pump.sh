016a45e4cd1611613bdd866d4209c82b16f5aa44
patrocinio <epatro@gmail.com>
2019-10-10 17:39:31 -0400

    Fix pump

--- scripts/pump.sh
+++ scripts/pump.sh
@@ -1,15 +1,27 @@
-CURL=/usr/local/opt/curl/bin/curl
+#CURL=/usr/local/opt/curl/bin/curl
+CURL=curl
 
 function getPath {
   oc get route dispatcher --no-headers=true | awk '{print $2}'
 }
 
-function pump {
-  $CURL $PATH/pump
+function getPort {
+  	port=$(kubectl get -o jsonpath="{.spec.ports[0].nodePort}" services $1)
+	echo $port
+}
+
+function sendRequest {
+  $CURL $NODE:$TARGET/$1
 }
 
 
-PATH=$(getPath)
-echo Path: $PATH
+
+function pump {
+  sendRequest pump
+}
+
+# OpenShift
+#PATH=$(getPath)
+#echo Path: $PATH
 
 pump
